8d545d4ce0b33c5ec87e7ef9df7d94a6b624969e
Momchil Mitev <momchilmitev87@gmail.com>
2020-08-15 19:12:55 +0200

    add simple db system with fs module and json file

--- phonebook.js
+++ phonebook.js
@@ -1,11 +1,30 @@
-let phoneBook = [];
+const fs = require('fs');
 
-const add = (contact) => phoneBook.push(contact);
+const db = {
+	fetchData(path) {
+		const data = fs.readFileSync(path, 'utf8', (err) => {});
+		return JSON.parse(data);
+	},
+
+	saveDate(data, path) {
+		const jsonData = JSON.stringify(data);
+		return fs.writeFile(path, jsonData, (err) => err);
+	},
+};
+
+let phoneBook = db.fetchData('./db.json');
+
+const add = (contact) => {
+	phoneBook.push(contact);
+	const err = db.saveDate(phoneBook, './db.json');
+	console.log(err);
+};
 
 const get = () => [...phoneBook];
 
 const deleteContact = (name) => {
 	phoneBook = phoneBook.filter((contact) => contact.name !== name);
+	db.saveDate(phoneBook, './db.json');
 };
 
 module.exports = {
