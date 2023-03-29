e76ddc317221a37fc711940805f5947b6bb27da2
momchilmitev <momchilmitev87@gmail.com>
2023-03-29 16:28:30 +0200

    generated the beasic form back btn component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackButtonComponent } from './subscription-form/components/back-button/back-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BackButtonComponent,
   ],
   imports: [
     BrowserModule,
